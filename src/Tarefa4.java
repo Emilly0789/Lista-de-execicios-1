315f1cfa2a060ed170712f37555bb1ddaa115944
Emilly0789 <emilly.pinheiro@academico.ifpb.edu.br>
2026-07-28 17:08:33 -0300

    Tarefa5
    questão 6

---
+++ src/Tarefa4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Tarefa4{
+    public static void main (int args) {
+        Scanner sc = new Scanner(System.in);
+        int contador = 0;
+        while (contador <= 5);
+
+        System.out.println("Contador " + contador);
+    }
+}
