3c444e9b6a5bafbdf7a5e750e2dd860b5ab0df52
Emilly0789 <emilly.pinheiro@academico.ifpb.edu.br>
2026-07-28 17:12:01 -0300

    Update Tarefa4.java
    Questão 6 atualizada

--- src/Tarefa4.java
+++ src/Tarefa4.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 
-class Tarefa4{
-    public static void main (int args) {
+public class Tarefa4 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int contador = 0;
-        while (contador <= 5);
 
-        System.out.println("Contador " + contador);
+        while (contador <= 5) {
+            System.out.println("Contador " + contador);
+            contador++;
+        }
+
+        sc.close();
     }
 }
